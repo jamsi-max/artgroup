61fdb11e31425032d62c35d6fdbf4a41db0bc75f
device <a38@Device.local>
2024-11-08 17:23:25 +0300

    home+about+portfolio fixed bug

--- js/app.js
+++ js/app.js
@@ -104,22 +104,22 @@ function popupBox(){
 // END POPUP BOX
 
 // PORTFOLIO FIX BACKGROUND
-const showDialog = () => {
-    document.getElementById('popup').classList.add('show')
-    const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
-    const body = document.body;
-    body.style.position = 'fixed';
-    body.style.top = `-${scrollY}`;
-};
-const closeDialog = () => {
-    const body = document.body;
-    const scrollY = body.style.top;
-    body.style.position = '';
-    body.style.top = '';
-    window.scrollTo(0, parseInt(scrollY || '0') * -1);
-    document.getElementById('popup').classList.remove('show');
-}
-window.addEventListener('scroll', () => {
-    document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
-});
+// const showDialog = () => {
+//     document.getElementById('popup').classList.add('show')
+//     const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+//     const body = document.body;
+//     body.style.position = 'fixed';
+//     body.style.top = `-${scrollY}`;
+// };
+// const closeDialog = () => {
+//     const body = document.body;
+//     const scrollY = body.style.top;
+//     body.style.position = '';
+//     body.style.top = '';
+//     window.scrollTo(0, parseInt(scrollY || '0') * -1);
+//     document.getElementById('popup').classList.remove('show');
+// }
+// window.addEventListener('scroll', () => {
+//     document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+// });
 // END PORTFOLIO FIX BACKGROUND
