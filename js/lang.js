d98d0fdb3a6c1717fc1246ab847615e1226aa2fe
device <a38@Device.local>
2024-11-19 21:32:33 +0300

    add translate

--- js/lang.js
+++ js/lang.js
@@ -244,8 +244,200 @@ const langList = {
            and urban planning decision) and other documents necessary for the design and commissioning\
             of the facility.",
     },
+    "services-1-calc" : {
+        "ru": "Сделать расчет стоимости",
+        "en": "Calculate the work",
+    },
 
 
+    "services-2-heading" : {
+        "ru": "Почему мы?",
+        "en": "Why us?",
+    },
+    "services-2-title-1" : {
+        "ru": "Эффективность",
+        "en": "Effectiveness",
+    },
+    "services-2-text-1" : {
+        "ru": "В основе нашей работы - эффективная организация пространства и использование BIM - технологий. 30-летний опыт работы и слаженная команда позволяет добиваться необходимых результатов в сжатые сроки.",
+        "en": "Our work is based on the effective organization of space and the use of BIM technologies. 30 years of experience and a well-coordinated team allows us to achieve the necessary results in a short time.",
+    },
+    "services-2-title-2" : {
+        "ru": "Творчество",
+        "en": "Creativity",
+    },
+    "services-2-text-2" : {
+        "ru": "Мы считаем, что любой проект, будь технологий жилой комплекс или новый аэропорт, помимо эффективных решений, должен обладать яркими, запоминающимся образом, формипующим лицо города.",
+        "en": "We believe that any project, be it technology a residential complex or a new airport, In addition to effective solutions, it must have bright, memorable in a way that shapes the face of the city.",
+    },
+    "services-2-title-3" : {
+        "ru": "Партнерство",
+        "en": "Partnership",
+    },
+    "services-2-text-3" : {
+        "ru": "Кооперация и взаимодействие с различными экспертами и проектными компаниями - конструкторами, дизайнерами, инженерами, экономистами, социологами - позволяет решать одновременно ряд крупных проектных задач.",
+        "en": "Cooperation and interaction with various experts and design companies - designers, designers, engineers, economists, sociologists - allows us to simultaneously solve a number of major design tasks.",
+    },
+    "partners-heading" : {
+        "ru": "Наши партнеры",
+        "en": "Our partners",
+    },
+    "portfolio-heading" : {
+        "ru": "Наши работы",
+        "en": "Portfolio",
+    },
+    "load-more-btn" : {
+        "ru": "Ещё работы",
+        "en": "More work",
+    },
+
+    
+    "portfolio-1-title" : {
+        "ru": "Концепция ЖК «Будь счастлив!»",
+        "en": "The concept of the complex «Be happy!»",
+    },
+    "portfolio-1-text-1" : {
+        "ru": "Проект жилого комплекса",
+        "en": "Residential complex project",
+    },
+    "portfolio-1-text-2" : {
+        "ru": "Архитектурно-юридическая компания<br>«ART group»",
+        "en": "Architectural law company<br>«ART group»",
+    },
+    "portfolio-1-btn-more" : {
+        "ru": "Подробнее",
+        "en": "More",
+    },
+    "portfolio-1-subtext-1" : {
+        "ru": "<b>Концепция ЖК «Будь счастлив!»</b>",
+        "en": "<b>The concept of the residential complex «Be happy!»</b>",
+    },
+    "portfolio-1-subtext-2" : {
+        "ru": "<b>Адрес объекта:</b> г. Ярославль",
+        "en": "<b>The address of the object:</b> Yaroslavl",
+    },
+    "portfolio-1-subtext-3" : {
+        "ru": "<b>Заказчик: ИК «Альтернатива»</b>",
+        "en": "<b>Customer: «Alternative»</b>",
+    },
+    "portfolio-1-subtext-4" : {
+        "ru": "Концепция разработана с учетом авторской методики генерального директора ООО «АРТ групп» <b><a href='#director1' class='popup-link-contact'>Елизаветы Морозовой</a></b> «Счастливый город», основанный на книге «PRO счастье», в которой собрано множество научных данных, исследований и подходов мировых аналогов по строительству жилых кварталах, в которых люди чувствуют себя более счастливыми и реализованными.",
+        "en": "The concept was developed taking into account the author's methodology of the General Director of ART Group LLC <b><a href='#director1' class='popup-link-contact'>Elizaveta Morozova</a></b> «Happy City», based on the book «PRO happiness», which contains There is a lot of scientific data, research and approaches of world analogues for the construction of residential neighborhoods in which people feel happier and more fulfilled.",
+    },
+    "portfolio-1-subtext-5" : {
+        "ru": "Наш подход базируется не только на создании качественной и элегантной архитектуры, но и на сценариях развития сообщества внутри квартала, что, на наш взгляд может стать на первое место в иерархии комфортного проживания людей",
+        "en": "Our approach is based not only on creating high-quality and elegant architecture, but also on scenarios for the development of a community within a quarter, which, in our opinion, can take the first place in the hierarchy of comfortable living for people",
+    },
+    "portfolio-1-subtext-6" : {
+        "ru": "Одна из основных проблем любого жилого квартала, как старой , так и новой формации, сталкивается с одними и теми же проблемами: люди, переезжая в свои квартиры, вечно чем-то недовольны, в чатах жителей наблюдается множество негатива, несмотря на то, что архитектура зданий, красиво создает градостроительный образ, словно повторяя 3 модель, нарисованную в концепции",
+        "en": "One of the main problems of any residential quarter, both old and new formations, faces the same problems: people moving into their apartments are always dissatisfied with something, there is a lot of negativity in residents' chats, despite the fact that the architecture of buildings beautifully creates an urban image, as if repeating 3 the model drawn in the concept",
+    },
+    "portfolio-1-subtext-7" : {
+        "ru": "Но мы говорим, в первую очередь о людях, которые будут в этой архитектуре жить, любить, воспитывать людей, создавать бизнесы, гулять , наслаждаться жизнью. Для нас важно минимизировать отрицательный результат межличностного взаимодействия, который может появиться в процессе эксплуатации объекта",
+        "en": "But we are talking, first of all, about people who will live in this architecture, love, educate people, create businesses, walk, enjoy life. It is important for us to minimize the negative result of interpersonal interaction that may occur during the operation of the facility",
+    },
+    "portfolio-1-subtext-8" : {
+        "ru": "Концепция «Счастливый город» базируется как на общих, собранных нами, принципах социально-архитектурного кода, а также и частных потребностей человека, являющегося создателем и созидателем общества в целом и местных комьюнити, формирующимися согласно интересам отдельных групп",
+        "en": "The concept of a «Happy City» is based both on the general principles of the socio-architectural code that we have collected, as well as on the particular needs of a person who is the creator and creator of society as a whole and local communities that are formed according to the interests of individual groups",
+    },
+    "portfolio-1-subtext-9" : {
+        "ru": "<b>При желании ознакомиться с полной концепцией, напишите нам по <a href='#contact' class='popup-link-contact'>форме обратной связи!</a></b>",
+        "en": "<b>If you want to get acquainted with the full concept, write to us at <a href='#contact' class='popup-link-contact'>feedback form!</a></b>",
+    },
+
+
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
+    "nav-call" : {
+        "ru": "заказать звонок",
+        "en": "Please contact us",
+    },
     "nav-call" : {
         "ru": "заказать звонок",
         "en": "Please contact us",
